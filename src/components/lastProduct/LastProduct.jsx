d187c7a7dcd657b07cb280f0ebded7bd04b98147
santiagoBotina <santiagoboe04@gmail.com>
2022-03-21 00:01:19 -0500

    Arreglando problema de render con el componente del ultimo producto agregado

--- src/components/lastProduct/LastProduct.jsx
+++ src/components/lastProduct/LastProduct.jsx
@@ -1,55 +1,124 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, Component } from "react";
 import "./lastProduct.css";
 
-const Lastproduct = () => {
-  const [dataProduct, setDataProduct] = useState([]);
+class LastProduct extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: [],
+    };
+  }
 
-  useEffect(() => {
-    fetch("http://localhost:5000/api/products")
+  apiCall(url, res) {
+    fetch(url)
       .then((response) => response.json())
       .then((data) => {
-        setDataProduct(data.products);
+        res(data.products);
       });
-  }, []);
+  }
 
-  const findLastProducts = (arr) => {
-    let lastProduct;
-    arr.map((product) => {
-      if (product.idproducto >= arr[arr.length - 1].idproducto) {
-        lastProduct = product;
-      }
+  mostrarData = (data) => {
+    this.setState({
+      data: data[data.length - 1],
     });
-    return lastProduct;
   };
 
-  let dataProductFiltered = findLastProducts(dataProduct);
-  console.log(dataProductFiltered);
-  return (
-    <div className="productChart">
-      <h3>Último producto creado</h3>
-      <div className="item">
-        <h4 key={dataProductFiltered.nombre}>{dataProductFiltered.nombre}</h4>
-        <p key={dataProductFiltered.descripcion}>
-          {dataProductFiltered.descripcion}
-        </p>
+  componentDidMount() {
+    this.apiCall("http://localhost:5000/api/products", this.mostrarData);
+  }
 
-        <div className="buttons-product">
-          <form
-            action={`http://localhost:5000${dataProductFiltered.detail}`}
-            method="get"
-          >
-            <button type="submit" className="verBoton">
-              Ir a ver
-            </button>
-          </form>
-          <img
-            src={`http://localhost:5000/images/our_products/${dataProductFiltered.image}`}
-            alt="user-image"
-            width={300}
-          />
+  render() {
+    console.log(this.state.data);
+
+    let dataDisplay;
+
+    if (this.state.data == []) {
+      dataDisplay = <p>Cargando...</p>;
+    } else {
+      dataDisplay = (
+        <div className="productChart">
+          <h3>Último producto creado</h3>
+          <div className="item">
+            <h4 key={this.state.data.nombre}>{this.state.data.nombre}</h4>
+            <p key={this.state.data.descripcion}>
+              {this.state.data.descripcion}
+            </p>
+
+            <div className="buttons-product">
+              <form
+                action={`http://localhost:5000${this.state.data.detail}`}
+                method="get"
+              >
+                <button type="submit" className="verBoton">
+                  Ir a ver
+                </button>
+              </form>
+              <img
+                src={`http://localhost:5000/images/our_products/${this.state.data.image}`}
+                alt="user-image"
+                width={200}
+              />
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
-  );
-};
-export default Lastproduct;
+      );
+    }
+
+    return <div>{dataDisplay}</div>;
+  }
+}
+
+export default LastProduct;
+
+// const Lastproduct = () => {
+//   const [dataProduct, setDataProduct] = useState([]);
+
+//   useEffect(() => {
+//     fetch("http://localhost:5000/api/products")
+//       .then((response) => response.json())
+//       .then((data) => {
+//         setDataProduct(data.products);
+//       });
+//   }, []);
+
+// const findLastProducts = (arr) => {
+//   let lastProduct;
+//   arr.map((product) => {
+//     if (product.idproducto >= arr[arr.length - 1].idproducto) {
+//       lastProduct = product;
+//     }
+//   });
+//   return lastProduct;
+// };
+
+// let dataProductFiltered = findLastProducts(dataProduct);
+//   console.log(dataProductFiltered);
+//   return (
+//     <div className="productChart">
+// <h3>Último producto creado</h3>
+// <div className="item">
+//   <h4 key={dataProductFiltered.nombre}>{dataProductFiltered.nombre}</h4>
+//   <p key={dataProductFiltered.descripcion}>
+//     {dataProductFiltered.descripcion}
+//   </p>
+
+//   <div className="buttons-product">
+//     <form
+//       action={`http://localhost:5000${dataProductFiltered.detail}`}
+//       method="get"
+//     >
+//       <button type="submit" className="verBoton">
+//         Ir a ver
+//       </button>
+//     </form>
+//     <img
+//       src={`http://localhost:5000/images/our_products/${dataProductFiltered.image}`}
+//       alt="user-image"
+//       width={300}
+//     />
+//   </div>
+// </div>
+//     </div>
+//   );
+// };
+// export default Lastproduct;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter, Link, Route, Switch } from "react-router-dom";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
 import Nav from "./components/navbar/Nav";
@@ -7,13 +8,13 @@ import Sidebar from "./components/Sidebar/Sidebar";
 import Home from "./pages/home/Home";
 
 ReactDOM.render(
-  <React.StrictMode>
+  <BrowserRouter>
     <Nav contacto="Santiago" buscar="Hola Prop" nosotros="Hola Prop 2" />
     <div className="wrapper">
       <Sidebar className="sidebar" />
       <Home />
     </div>
-  </React.StrictMode>,
+  </BrowserRouter>,
   document.getElementById("root")
 );
 
